6f1b82affcb5fb4950cca83a120075468cdd00bb
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2021-06-16 08:42:06 +0300

    add slider in projecty.html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper('.projecty__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+        el: '.swiper-pagination',   
+        type: 'fraction'   
+    },
+});
