079c61ffe9a83110689d87a1dd958155a30191ac
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2021-06-27 14:53:41 +0300

    chenged decor-line and decor-word

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
-new Swiper('.projecty__slider', {    
-    slidesPerView: 3,
-    spaceBetween: 161, 
-    centeredSlides: true,
-    loop: true,
+// new Swiper('.projecty__slider', {    
+//     slidesPerView: 3,
+//     spaceBetween: 161, 
+//     centeredSlides: true,
+//     loop: true,
     
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    pagination: {
-        el: '.swiper-pagination',   
-        type: 'fraction'   
-    },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     pagination: {
+//         el: '.swiper-pagination',   
+//         type: 'fraction'   
+//     },
+// });
+
+let popUp = document.querySelector('.popup');
+let btn = document.querySelector('.btn-popup');
+let popUpClose = document.querySelector('.popup__close')
+let footerBtn = document.querySelector('.btn-dark');
+
+function showPopUpOnClick(){
+    popUp.classList.add('popup-show');
+}
+
+btn.addEventListener('click', showPopUpOnClick);
+
+footerBtn.addEventListener('click', showPopUpOnClick);
+popUpClose.addEventListener('click', ()=>{
+    popUp.classList.remove('popup-show');
 });
-
-
